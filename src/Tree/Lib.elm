449875782fc32b17bf51b55eba7fa73f7e7053df
James Carlson <jxxcarlson@gmail.com>
2021-11-29 11:32:13 -0500

    Move code from Render to LIb

--- src/Tree/Lib.elm
+++ src/Tree/Lib.elm
@@ -1,6 +1,7 @@
-module Tree.Lib exposing (preorder)
+module Tree.Lib exposing (preorder, test, toString)
 
 import Tree exposing (Tree)
+import Tree.Build exposing (InitialData)
 import Tree.Zipper as Zipper exposing (Zipper)
 
 
@@ -49,6 +50,46 @@ nextStep state =
 
                 Just newZipper ->
                     Loop { zipper = Just newZipper, nodes = Zipper.label newZipper :: state.nodes }
+
+
+toString : Int -> (a -> String) -> Tree a -> String
+toString quantum renderNode tree =
+    renderAux quantum renderNode ( tree, 0 ) |> String.dropRight 1
+
+
+test : Int -> (node -> String) -> InitialData node -> String -> String
+test quantum renderNode initialData str =
+    let
+        maybeStr2 =
+            Tree.Build.fromString initialData str |> Result.toMaybe |> Maybe.map (toString quantum renderNode)
+    in
+    if Just (String.trim str) == maybeStr2 then
+        "Ok"
+
+    else
+        case maybeStr2 of
+            Nothing ->
+                "Failed to build tree or render it to string"
+
+            Just str2 ->
+                String.trim str ++ " ≠ " ++ str2
+
+
+renderAux : Int -> (node -> String) -> ( Tree node, Int ) -> String
+renderAux quantum renderNode ( tree, level ) =
+    let
+        children =
+            List.map (\c -> ( c, level + 1 )) (Tree.children tree)
+    in
+    prefix quantum level
+        ++ renderNode (Tree.label tree)
+        ++ "\n"
+        ++ String.join "" (List.map (renderAux quantum renderNode) children)
+
+
+prefix : Int -> Int -> String
+prefix quantum level =
+    String.repeat (quantum * level) " "
 
 
 type Step state a
