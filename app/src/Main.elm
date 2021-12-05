1496bf6de03e981b1e8cf03853a02657a61f6d55
James Carlson <jxxcarlson@gmail.com>
2021-12-05 13:45:40 -0500

    set up editor in app

--- app/src/Main.elm
+++ app/src/Main.elm
@@ -1,11 +1,92 @@
-module Main exposing (..)
+module Main exposing (main)
 
+import Browser
+import Browser.Dom as Dom
+import Element exposing (..)
+import Element.Background as Background
+import Element.Font as Font
+import Element.Input as Input
+import File.Download as Download
+import Html exposing (Html)
+import Html.Attributes as HtmlAttr exposing (attribute)
+import Html.Events exposing (keyCode, on, onClick, onInput)
+import Json.Decode
+import Process
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Task exposing (Task)
 import Tree.Build as Build
+import Tree.Graph
 import Tree.Svg
 import Tree.Transform exposing (defaults)
 import Tree.TransformR as TR
+
+
+type alias Model =
+    { sourceText : String
+    , windowHeight : Int
+    , windowWidth : Int
+    , message : String
+    , lineNumber : Int
+    , graph : Maybe Tree.Graph.Graph
+    }
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Flags =
+    { width : Int, height : Int }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( { sourceText = ""
+      , windowHeight = flags.height
+      , windowWidth = flags.width
+      , message = ""
+      , lineNumber = 0
+      , graph = Nothing
+      }
+    , Cmd.none
+    )
+
+
+type Msg
+    = NoOp
+    | InputText String
+    | ClearText
+
+
+subscriptions model =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        InputText t ->
+            ( { model
+                | sourceText = t
+              }
+            , Cmd.none
+            )
+
+        ClearText ->
+            ( { model
+                | sourceText = ""
+              }
+            , Cmd.none
+            )
 
 
 tree1 =
@@ -96,22 +177,164 @@ preferences =
     { defaults | ballRadius = 10, halfAngle = 0.1 * pi, scaleFactor = 0.85 }
 
 
-main =
+render : Tree.Graph.Graph -> Html msg
+render graph_ =
     svg
-        [ width "900"
-        , height "900"
-        , viewBox "0 0 1000 1000"
+        [ Svg.Attributes.width "900"
+        , Svg.Attributes.height "900"
+        , Svg.Attributes.viewBox "0 0 1000 1000"
         ]
         ([ rect
-            [ x "10"
-            , y "10"
-            , width "100"
-            , height "100"
-            , rx "15"
-            , ry "15"
-            , fill "white"
+            [ Svg.Attributes.x "10"
+            , Svg.Attributes.y "10"
+            , Svg.Attributes.width "100"
+            , Svg.Attributes.height "100"
+            , Svg.Attributes.rx "15"
+            , Svg.Attributes.ry "15"
+            , Svg.Attributes.fill "white"
             ]
             []
          ]
-            ++ Tree.Svg.render Tree.Svg.FullLabel (Tree.Svg.transform 480 100 60 60 0.5 graph1)
+            ++ Tree.Svg.render Tree.Svg.FullLabel (Tree.Svg.transform 480 100 60 60 0.5 graph_)
         )
+
+
+view : Model -> Html Msg
+view model =
+    Element.layoutWith { options = [ focusStyle noFocus ] } [ bgGray 0.2, clipX, clipY ] (mainColumn model)
+
+
+mainColumnStyle model =
+    [ centerX
+    , centerY
+    , bgGray 0.5
+    , paddingXY 20 20
+    , Element.width (px (appWidth_ + 40))
+    , Element.height (px (appHeight_ model + 40))
+    ]
+
+
+rhs : Model -> Element Msg
+rhs model =
+    let
+        toRender =
+            case model.graph of
+                Nothing ->
+                    [ Element.text "No valid graph" ]
+
+                Just g ->
+                    [ render g |> Element.html ]
+    in
+    column [ Element.spacing 8 ]
+        [ row
+            [ fontGray 0.9
+            , Element.spacing 12
+            , moveUp 9
+            , Font.size 14
+            ]
+            toRender
+        ]
+
+
+editor_ : Model -> Element Msg
+editor_ model =
+    let
+        onChange : Html.Attribute Msg
+        onChange =
+            Json.Decode.string
+                |> Json.Decode.at [ "target", "editorText" ]
+                |> Json.Decode.map InputText
+                |> Html.Events.on "change"
+    in
+    el [ htmlAttribute onChange ] <|
+        html <|
+            Html.node "ace-editor"
+                [ HtmlAttr.attribute "theme" "twilight"
+                , HtmlAttr.attribute "wrapmode" "true"
+                , HtmlAttr.attribute "tabsize" "2"
+                , HtmlAttr.attribute "linenumber" (String.fromInt (model.lineNumber + 1))
+                , HtmlAttr.attribute "softtabs" "true"
+                , HtmlAttr.attribute "navigateWithinSoftTabs" "true"
+                , HtmlAttr.attribute "fontsize" "12"
+                , HtmlAttr.style "height" (String.fromInt (innerPanelHeight model) ++ "px")
+                , HtmlAttr.style "width" (String.fromInt panelWidth_ ++ "px")
+                , HtmlAttr.attribute "text" model.sourceText
+                ]
+                []
+
+
+
+-- PARAMETERS
+
+
+widePanelWidth_ =
+    2 * panelWidth_
+
+
+panelWidth_ =
+    560
+
+
+appHeight_ model =
+    model.windowHeight - 140
+
+
+panelHeight model =
+    px (appHeight_ model - 160)
+
+
+innerPanelHeight model =
+    appHeight_ model - 180
+
+
+appWidth_ =
+    2 * panelWidth_ + 15
+
+
+editor model =
+    column [ Element.height (px (innerPanelHeight model)), moveUp 28 ]
+        [ row [ Element.spacing 12 ]
+            []
+        , editor_ model
+        ]
+
+
+noFocus : Element.FocusStyle
+noFocus =
+    { borderColor = Nothing
+    , backgroundColor = Nothing
+    , shadow = Nothing
+    }
+
+
+mainColumn : Model -> Element Msg
+mainColumn model =
+    column (mainColumnStyle model)
+        [ column [ centerY, paddingEach { top = 46, bottom = 0, left = 0, right = 0 }, Element.spacing 8, Element.width (px appWidth_), Element.height (px (appHeight_ model)) ]
+            [ -- title "L3 Demo App"
+              column [ Element.height (panelHeight model), Element.spacing 12 ]
+                [ row [ Element.spacing 12 ] [ editor model, rhs model ]
+                ]
+            , row [ Element.paddingXY 8 0, Element.height (px 30), Element.width Element.fill, Font.size 14, Background.color (Element.rgb 0.3 0.3 0.3), Font.color (Element.rgb 1 1 1) ] [ Element.text model.message ]
+            ]
+        ]
+
+
+
+-- STYLE
+
+
+grayColor g =
+    Element.rgb g g g
+
+
+whiteColor =
+    grayColor 1
+
+
+fontGray g =
+    Font.color (Element.rgb g g g)
+
+
+bgGray g =
+    Background.color (Element.rgb g g g)
